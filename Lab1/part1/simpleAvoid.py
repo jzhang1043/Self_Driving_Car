0af40cf8642bc7aacd91d4e74f55a89d21bdcfc7
jzhang1043 <jzhang1043@gmail.com>
2022-10-19 15:17:01 -0500

    simpleAvoid.py

--- Lab1/part1/simpleAvoid.py
+++ Lab1/part1/simpleAvoid.py
@@ -2,6 +2,7 @@ import picar_4wd as fc
 import random
 import time
 
+
 power = 30
 
 def main():
@@ -9,21 +10,17 @@ def main():
         scan = fc.scan_step(30)
         if not scan:
             continue
-        print(scan)
+        # status is 108 degree in front of the car
         status = scan[2:8]
         # print(status)  
         if status != [2,2,2,2,2,2]:
-            fc.stop()
-            fc.get_distance_at(0)
-            time.sleep(0.2)
             fc.backward(power)
-            time.sleep(0.2)
-            fc.stop()
-            time.sleep(0.2)
+            fc.get_distance_at(0)
+            time.sleep(0.3)
             
             k = random.randint(0, 1)
             if k == 0:
-                fc.turn_left(power)
+                fc.turn_left(power)           
             else:
                 fc.turn_right(power)
         else:
